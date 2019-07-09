4ab4ce8d4b506d89ab035aeacf4959ad8163e477
zhaopp <nicai@qq.com>
2019-07-09 10:43:56 +0800

    two

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <p>App Components</p>
+  <div id="app">
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -13,6 +18,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style lang="less" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-import axios from 'axios'
+import router from './router'
+
+// 安装vant
 import Vant from 'vant'
 import 'vant/lib/index.css'
-import 'amfe-flexible/index.js'
+import 'amfe-flexible'
 
 Vue.use(Vant)
 
 // axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
-axios.defaults.baseURL = 'http://toutiao.course.itcast.cn'
+// axios.defaults.baseURL = 'http://toutiao.course.itcast.cn'
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
